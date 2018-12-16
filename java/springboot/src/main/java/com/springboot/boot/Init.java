e12b27adb4f655bdaa70ae363ff3ad07e4082117
Nott <1564223954@qq.com>
2018-12-16 23:43:14 +0800

    update

---
+++ java/springboot/src/main/java/com/springboot/boot/Init.java
@@ -0,0 +1,13 @@
+package com.springboot.boot;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class Init implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// 在这里写入初始化时运行的代码
+	}
+
+}
